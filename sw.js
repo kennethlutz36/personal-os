dec31391f3f225485876c415a38f5f981721332d
kennethlutz36 <kennethlutz36@gmail.com>
2026-08-18 18:41:48 -0700

    Activate Personal OS v21 To-Do cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='personal-os-v20-calendar-20260818';
+const CACHE='personal-os-v21-todo-20260818';
 const CORE=['./','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const ks=await caches.keys();await Promise.all(ks.filter(k=>k.startsWith('personal-os')&&k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim()})())});
